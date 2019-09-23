ca7ea73ddf7db8083077755269f8934ecdc7e036
Luis Castro Martín <luiscastro193@gmail.com>
2019-09-23 19:02:54 +0200

    More robust regex

--- condorcet.js
+++ condorcet.js
@@ -71,6 +71,6 @@ let ballots = `
 	3 1 2
 `;
 
-ballots = ballots.trim().split('\n').map(ballot => ballot.trim().split(' ').map(rank => parseInt(rank)));
+ballots = ballots.trim().split(/[\n\r]+/).map(ballot => ballot.trim().split(/[^0-9]+/).map(rank => parseInt(rank)));
 
 console.log(condorcetIrvWinner(condorcetMatrix(ballots), ballots));
